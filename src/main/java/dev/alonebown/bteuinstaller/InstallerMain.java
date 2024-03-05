63bc4699f4552f7d3d925d062edb6867ea6adfcb
AloneBown <74094680+AloneBown@users.noreply.github.com>
2024-03-05 21:00:54 +0200

    Update InstallerMain class and MANIFEST.MF

--- src/main/java/dev/alonebown/bteuinstaller/InstallerMain.java
+++ src/main/java/dev/alonebown/bteuinstaller/InstallerMain.java
@@ -1,9 +1,19 @@
 package dev.alonebown.bteuinstaller;
 
-import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.FlatGitHubIJTheme;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class InstallerMain{
+    public static void main(String[] args){
+        FlatGitHubIJTheme.install(new FlatGitHubIJTheme()); // Install the theme
+        final JFrame frame = new JFrame("BTE Ukraine Installer");
+    }
+}
