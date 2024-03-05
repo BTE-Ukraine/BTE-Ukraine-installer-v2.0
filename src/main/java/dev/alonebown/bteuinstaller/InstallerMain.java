2d75bc1d5c820f192097b3e75e67378e3167e043
AloneBown <74094680+AloneBown@users.noreply.github.com>
2024-03-05 20:47:32 +0200

    Remove unnecessary files and configurations

---
+++ src/main/java/dev/alonebown/bteuinstaller/InstallerMain.java
@@ -0,0 +1,9 @@
+package dev.alonebown.bteuinstaller;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import java.awt.*;
